486509d7acf6dcff03149de7c4ca299fbf3664ec
andrey <yaidf@ya.ru>
2025-11-12 17:38:51 +0300

    cuda to cpu

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from core.RagContext import RagContext
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 MAIN_EMBEDDER = HuggingFaceEmbeddings(
     model_name='ai-sage/Giga-Embeddings-instruct',
-    model_kwargs={'device': 'cuda', "trust_remote_code": True},
+    model_kwargs={'device': 'cpu', "trust_remote_code": True},
     encode_kwargs={'normalize_embeddings': True},
 )
 
